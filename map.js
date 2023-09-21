88e9848b8aa919136441df3c2b572ca168085fee
Kaspar Vakkum <apelsin.kv@gmail.com>
2023-09-21 11:47:45 +0300

    tailwindi lisamine ja mapi alustamine

---
+++ map.js
@@ -0,0 +1,18 @@
+var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: { lat: 58.26659964738753, lng: 22.536134881926614 } // ametikool
+  });
+    window.initMap = initMap;  
+  
+  // add marker
+  var marker1 = new google.maps.Marker({
+    position: { lat: 58.26659964738753, lng: 22.536134881926614 },
+    map: map,
+    title: 'Kudjape jäätmejaam'
+  });
+  
+  var marker2 = new google.maps.Marker({
+    position: { lat: 58.5582415707529, lng: 23.0827982972879 },
+    map: map,
+    title: 'Marker 2'
+  });
